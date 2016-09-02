035da0b2f62d292ce8bf16834c78b9b1d564ea55
Zach Roberts <zach.ker.roberts@gmail.com>
2016-09-02 14:25:57 -0400

    asking for help for ajax reloading of specific div

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,14 +1,26 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on("ready", function(){
   $(".submit_1").on("click", function(e){
     e.preventDefault();
     $.ajax({
       url: '/',
-      method: 'post',
-      data: {results: $(".search_1").val()}
-      // success: function(){
+      data: {results: $(".search_1").val()},
+      method: 'post'
+      // success: function(data){
       //   $(".results").load("/ .results")
       // }
+      // console.log(typeOf(data));
+      // $(".results").html(data);
+    // }
     })
+    $('.results').on('ajax:success', function(){
+      console.log("this")
+      $(this).load('/ .results')
+    });
   })
+
+
+   });
+})
--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,7 @@
 class HomepageController < ApplicationController
   def home
-    @results = []
     if params["results"]
     @results = Character.where("LOWER(name) LIKE LOWER('%#{params["results"]}%')")
-    p @results
     end
   end
 end
